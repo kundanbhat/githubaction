e535c0aaac5f5d8c79729acead2dcc3d0b15165e
kundanbhatbq <kundan.bhat@bqprime.com>
2025-03-06 11:48:49 +0530

    Initial commit

---
+++ src/main/java/com/example/test/githubaction/GithubactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionApplication.class, args);
+	}
+
+}
